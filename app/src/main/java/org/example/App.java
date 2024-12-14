21374e7401268141534509d80cc49e7f5e3254ff
Natasha <darteynatasha409@gmail.com>
2024-12-14 20:34:09 +0000

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,23 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        //Creating 2 instances of the book class
+        Book book1 = new Book("love in the deep", "Sylus", 2024 );
+        Book book2 = new Book("The Extraction", "Cyril Banks", 2006 );
+
+
+        // Creating an instance of the library to store the 2 books above
+        Library library = new Library();
+
+        // Adding book1 and book2 to library1
+        library.addBook(book1);
+        library.addBook(book2);
+
+
+        // Listing all the books in library1
+        System.out.println(library);
+
     }
 }
